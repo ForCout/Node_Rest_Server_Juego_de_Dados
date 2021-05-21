894597b56a097a6823a7d2d70196b88f0c2b7fc0
forcout <monchoforcout@gmail.com>
2021-05-21 17:46:51 +0200

    ww

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,28 +11,28 @@ router.post('/players', controllerJugador.insertJugador);
 
 
 // Modifica el nom del jugador
- router.put('/players',auth, controllerJugador.actualizaNombre);
+ router.put('/players', controllerJugador.actualizaNombre);
 
 // Un jugador específic realitza una tirada dels daus.
-router.post("/players/:id/games",auth, controller.tiradaDados);
+router.post("/players/:id/games", controller.tiradaDados);
 
 // Elimina les tirades del jugador
-router.delete("/players/:id/games",auth, controller.deletePartidas);
+router.delete("/players/:id/games", controller.deletePartidas);
 
 //Retorna el llistat de tots els jugadors del sistema amb el seu percentatge mig d’èxits
-router.get("/players",auth, controller.player);
+router.get("/players", controller.player);
 
 //Retorna el llistat de jugades per un jugador.
-router.get("/players/:id/games",auth, controller.listaPartidas);
+router.get("/players/:id/games", controller.listaPartidas);
 
 //Retorna el ranking mig de tots els jugadors del sistema. 
-router.get("/players/ranking",auth, controller.allRanking);
+router.get("/players/ranking", controller.allRanking);
 
 //Retorna el jugador amb pitjor percentatge d’èxit
-router.get("/players/ranking/loser",auth, controller.loser);
+router.get("/players/ranking/loser", controller.loser);
 
 //Retorna el jugador amb millor percentatge d’èxit
-router.get("/players/ranking/winner",auth, controller.winner);
+router.get("/players/ranking/winner", controller.winner);
 
 //Ruta para hacer login y generar token 
 router.post("/login", controllerAuth)
