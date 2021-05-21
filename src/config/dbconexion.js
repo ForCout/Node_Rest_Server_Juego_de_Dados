59b5fd237ef884557ff009678491e301ee5c68d8
forcout <monchoforcout@gmail.com>
2021-05-22 01:08:04 +0200

    style

--- src/config/dbconexion.js
+++ src/config/dbconexion.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
-
 mongoose
-    .connect('mongodb://localhost:27017/dados', { 
+    .connect('mongodb://localhost:27017/dados', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify: false 
-})
+        useFindAndModify: false,
+    })
     .then(() => console.log('conexion establecida con MongoDB'))
-    .catch(error => console.error('No se ha podido conectar con MongoDB:', error.message))
+    .catch((error) =>
+        console.error('No se ha podido conectar con MongoDB:', error.message)
+  );
 
 const db = mongoose.createConnection();
 
 module.exports = {
-    
-    SECRET_TOKEN: 'mitokenseguro'
-}
+  SECRET_TOKEN: 'mitokenseguro',
+};
